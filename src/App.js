0fb1152381996339d4806c2bd8bdf196f584ca19
TimLee <xodnjs479@gmail.com>
2022-03-27 14:52:14 +0900

    ReactJS Toy Project myblog 만들기 - NavLayout 제작 및 페이지 라우터처리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    "hello"
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../_layout/layout";
+
+const Home = () => {
+    return(
+        <Layout>
+            <div>Home</div>
+        </Layout>
+    )
+}
+
+export default Home;
